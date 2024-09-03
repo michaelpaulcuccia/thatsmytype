d09b259e782258c7e6dc5cfb153b8d94ca6b7f13
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-03 15:11:58 -0500

    cleans up page

---
+++ MyKendoApp/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Calendar } from "@progress/kendo-react-dateinputs";
+
+export default function page() {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+}
