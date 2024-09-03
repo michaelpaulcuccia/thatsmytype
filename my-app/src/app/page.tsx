2414216a2886021c6f49589e3f161b39bd510f65
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-03 15:56:14 -0500

    started pizza app

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,18 +1,57 @@
 "use client";
 import React from "react";
-import { Calendar } from "@progress/kendo-react-dateinputs";
 import styled from "styled-components";
 
-const Test = styled.div`
-  background: blue;
-  color: white;
-`;
-
 export default function page() {
+  type MenuItem = {
+    name: string;
+    price: number;
+  };
+
+  const menu: MenuItem[] = [
+    { name: "Margherita", price: 14 },
+    { name: "Meat Lovers", price: 21 },
+    { name: "Veggie", price: 17 },
+  ];
+
+  let cashInRegister: number = 100;
+  let orderNumber: number = 0;
+  const orderQueue = [];
+
+  const Wrapper = styled.div`
+    display: flex;
+    padding: 24px;
+    grid-gap: 36px;
+  `;
+
+  const ItemBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    padding: 8px;
+  `;
+
   return (
-    <div>
-      <Calendar />
-      <Test>Hello World</Test>
-    </div>
+    <Wrapper>
+      <ItemBox>
+        <h2 style={{ marginBottom: "6px" }}>Menu</h2>
+        <div>
+          {menu.map((item, i) => (
+            <li key={i} style={{ marginBottom: "6px" }}>
+              {item.name}: ${item.price}
+            </li>
+          ))}
+        </div>
+      </ItemBox>
+      <ItemBox>
+        <div>Register Total: ${cashInRegister}</div>
+      </ItemBox>
+      <ItemBox>
+        Serving Order Number:{" "}
+        {orderNumber === 0 ? "no orders yet" : orderNumber}
+      </ItemBox>
+    </Wrapper>
   );
 }
