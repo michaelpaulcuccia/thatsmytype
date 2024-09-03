bc7a1ee7e127333d296fdb7114d5576af0e3d888
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-03 15:16:44 -0500

    works with styled-components

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import React from "react";
 import { Calendar } from "@progress/kendo-react-dateinputs";
+import styled from "styled-components";
+
+const Test = styled.div`
+  background: blue;
+  color: white;
+`;
 
 export default function page() {
   return (
     <div>
       <Calendar />
+      <Test>Hello World</Test>
     </div>
   );
 }
