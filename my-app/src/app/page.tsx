022bca42e0b14093b4bce89a5656c2962c5486b5
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-03 14:30:57 -0500

    set up with styled-components

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  background: blue;
+  color: white;
+`;
+
+export default function page() {
+  return <Root>page</Root>;
+}
