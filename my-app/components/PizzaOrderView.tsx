385a761b47ee5366823742983df1b04950667d14
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-04 10:21:34 -0500

    pizza order function, need context for register and serving displays

--- my-app/components/PizzaOrderView.tsx
+++ my-app/components/PizzaOrderView.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { usePizzaMenu } from "../context/MenuContext";
 
 export default function PizzaOrderView() {
-  return <div>PizzaOrderView</div>;
+  const { pizzaMenu } = usePizzaMenu();
+
+  function placeOrder(pizzaName: string) {
+    const selectedPizza = pizzaMenu.find((pizza) => pizza.name === pizzaName);
+    //get access to orderNumber
+    const newOrder = { id: 1, pizza: selectedPizza, status: "ordered" };
+    //update register
+    //return newOrder;
+    alert(JSON.stringify(newOrder, null, 2));
+  }
+
+  return (
+    <div>
+      <h2>Pizza Order View</h2>
+      <br />
+      {pizzaMenu.map((item, i) => (
+        <div
+          key={i}
+          onClick={() => placeOrder(item.name)}
+          style={{ marginBottom: "8px" }}
+        >
+          {item.name}: ${item.price}
+        </div>
+      ))}
+    </div>
+  );
 }
